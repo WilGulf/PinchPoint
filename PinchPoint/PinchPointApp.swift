2c8b34f7684d08e586dda30c80fcd5c17d67014c
WilGulf <wilgot.ulfstedt@outlook.com>
2026-07-24 13:55:40 +0300

    Initial Commit

---
+++ PinchPoint/PinchPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchPointApp.swift
+//  PinchPoint
+//
+//  Created by Wilgot Ulfstedt on 2026-07-24.
+//
+
+import SwiftUI
+
+@main
+struct PinchPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
